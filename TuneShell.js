4072735be4fbc30f769176cfc021ac5db650997d
sarthakcontrigit <e25b070715@adypu.edu.in>
2026-09-13 03:23:49 +0530

    Improved the show song function

--- TuneShell.js
+++ TuneShell.js
@@ -5,14 +5,29 @@ const songDirectory = path.join(__dirname, "Songs");
 const songs = fs.readdirSync(songDirectory).filter(song => song.toLowerCase().endsWith(".mp3"));
 // console.log(songs)
 
+let selectedIndex= 0;
+const LIST_SIZE = 10;
+
 function showSongs(songs){
+        console.clear();
+    let startIndex = 0;
+    if(startIndex >= LIST_SIZE){
+        startIndex = startIndex - LIST_SIZE + 1;
+
+    };
+    const songsToShow = songs.slice(startIndex, startIndex + LIST_SIZE);
     console.log("\n");
     console.log("-------------------- SONGS TO PLAY --------------------");
     console.log("\n");
-    songs.forEach((song, index)=>{
-        console.log(`${index+1}. ${song}`);
+ 
+        songsToShow.forEach((song, index)=>{
+        const actualIndex = startIndex + index;
+        const isSelected = actualIndex === selectedIndex;
+        const pointer = isSelected ? "--> " : "    ";
+        console.log(`${pointer}${actualIndex+1}. ${song}`);
     });
     console.log("\n");
+    console.log("Use ↑ / ↓ to navigate, Ctrl+C or 'q' to exit");
     console.log("-------------------------------------------------------");
 };
 
