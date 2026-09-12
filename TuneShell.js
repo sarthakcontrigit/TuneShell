e0ad9f255502f409b9d4118c40d9592a253a4b40
sarthakcontrigit <e25b070715@adypu.edu.in>
2026-09-13 03:19:37 +0530

    Added a function to show songs on application

--- TuneShell.js
+++ TuneShell.js
@@ -4,8 +4,21 @@ const path = require('path');
 const songDirectory = path.join(__dirname, "Songs");
 const songs = fs.readdirSync(songDirectory).filter(song => song.toLowerCase().endsWith(".mp3"));
 // console.log(songs)
+
+function showSongs(songs){
+    console.log("\n");
+    console.log("-------------------- SONGS TO PLAY --------------------");
+    console.log("\n");
+    songs.forEach((song, index)=>{
+        console.log(`${index+1}. ${song}`);
+    });
+    console.log("\n");
+    console.log("-------------------------------------------------------");
+};
+
+
 if(songs.length === 0){
     console.log("No songs in the Songs Directory!");
 }else{
-    console.log(songs);
+    showSongs(songs);
 };
